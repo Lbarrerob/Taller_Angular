faaf7d787d4a2c45d58128fab55a1ea4aba25a48
Lbarrerob <l.barrerob@uniandes.edu.co>
2024-11-11 19:25:14 -0500

    module Serie

---
+++ src/app/Serie/Serie.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SerieComponent } from './Serie.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SerieComponent]
+})
+export class SerieModule { }
